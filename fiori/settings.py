490d92c7f26553b8419123ee8f00425791abc7b2
gabriela-tomazzi <gabtomazzi@gmail.com.br>
2021-08-22 15:29:58 -0300

    var

--- fiori/settings.py
+++ fiori/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
